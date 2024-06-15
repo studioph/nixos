fcd09df0069f0baed40944e92ad10a2201cabdd5
Paul Hutchings <dev@studiop.page>
2024-06-15 14:56:26 -0400

    Switch to plasma6

--- framework16/system/configuration.nix
+++ framework16/system/configuration.nix
@@ -48,7 +48,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
