2abad609e19fb80266d2026e2a9fe9a9217a557c
Paul Hutchings <dev@studiop.page>
2024-10-10 11:04:27 -0400

    Change displayManager settings

--- framework16/system/configuration.nix
+++ framework16/system/configuration.nix
@@ -45,11 +45,13 @@
   };
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+#   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
+  services.displayManager.defaultSession = "plasma";
 
   # Configure keymap in X11
   services.xserver = {
