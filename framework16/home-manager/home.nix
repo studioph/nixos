c5fdc3fe81d14e075eee03f70e0cb19a1a6764c4
Paul Hutchings <dev@studiop.page>
2024-11-01 19:17:40 -0400

    Update mime defaults

--- framework16/home-manager/home.nix
+++ framework16/home-manager/home.nix
@@ -319,6 +319,26 @@
             "x-scheme-handler/http" = "firefox.desktop";
             "x-scheme-handler/https" = "firefox.desktop";
             "text/plain" = "org.kde.kate.desktop";
+            "audio/aac" = "vlc.desktop";
+            "audio/mp4" = "vlc.desktop";
+            "audio/mpeg" = "vlc.desktop";
+            "audio/mpegurl" = "vlc.desktop";
+            "audio/ogg" = "vlc.desktop";
+            "audio/vnd.rn-realaudio" = "vlc.desktop";
+            "audio/vorbis" = "vlc.desktop";
+            "audio/x-flac" = "vlc.desktop";
+            "audio/x-mp3" = "vlc.desktop";
+            "audio/x-mpegurl" = "vlc.desktop";
+            "audio/x-ms-wma" = "vlc.desktop";
+            "audio/x-musepack" = "vlc.desktop";
+            "audio/x-oggflac" = "vlc.desktop";
+            "audio/x-pn-realaudio" = "vlc.desktop";
+            "audio/x-scpls" = "vlc.desktop";
+            "audio/x-speex" = "vlc.desktop";
+            "audio/x-vorbis" = "vlc.desktop";
+            "audio/x-vorbis+ogg" = "vlc.desktop";
+            "audio/x-wav" = "vlc.desktop";
+            "x-scheme-handler/mailto" = "thunderbird.desktop";
         };
     };
 
