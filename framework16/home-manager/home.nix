28854b7d7eab9db04d83ecf4e13b9ec3d5713511
Paul Hutchings <dev@studiop.page>
2024-12-26 23:26:56 -0500

    Split packages config

--- framework16/home-manager/home.nix
+++ framework16/home-manager/home.nix
@@ -6,6 +6,9 @@ let
 
 in
 {
+    imports = [
+        ./pkgs.nix
+    ];
     # Home Manager needs a bit of information about you and the
     # paths it should manage.
     home = {
@@ -57,81 +60,7 @@ in
             target = ".config/pdm/config.toml";
         };
 
-        packages = (with pkgs; [
-            kdePackages.kate
-            python3
-            rustup
-            poetry
-            pdm
-            virt-manager
-            bitwarden
-            gomplate
-            marktext
-            kdePackages.krdc
-            ungoogled-chromium
-            talosctl
-            libreoffice-qt
-            ffmpeg
-            xdg-desktop-portal
-            kdePackages.xdg-desktop-portal-kde
-            kdePackages.kdeconnect-kde
-            nodejs_20
-            kdePackages.plasma-workspace-wallpapers
-            kdePackages.plasma-browser-integration
-            nerdfonts
-            nextcloud-client
-            kdePackages.kolourpaint
-            vlc
-            kdePackages.filelight
-            kdePackages.skanpage
-            protonmail-bridge-gui
-            kdePackages.kweather
-            vimix-cursors
-            papirus-icon-theme
-            tela-icon-theme
-            fluent-icon-theme
-            dotnetSDK # Defined as let var
-            xournalpp
-            kdePackages.kcalc
-            protonmail-bridge
-            qownnotes
-            libsForQt5.kamoso
-            nil
-            dino
-            kdePackages.merkuro
-            kdePackages.akonadi
-            kdePackages.kdepim-runtime
-            kdePackages.k3b
-            kdePackages.ksshaskpass
-            kid3
-            go-task
-            gtkterm
-            drawio
-            android-tools
-            opentofu
-            kdePackages.kdenlive
-            p7zip # TODO: Remove once Ark works without it
-            go-task
-            podman-tui
-            dive
-            podman-compose
-            kdePackages.isoimagewriter
-            kubernetes-helm
-            helmfile
-            kubernetes-helmPlugins.helm-diff
-            kubernetes-helmPlugins.helm-git
-            kubectl
-            pyenv
-            webcord
-            netcoredbg
-            hunspell
-            hunspellDicts.en_US
-            jetbrains.rider
-            rclone
-        ])
-        ++ (with unstable; [
-            dbgate
-        ]);
+
 
         sessionVariables = {
             DOTNET_ROOT = "${dotnetSDK}";
---
+++ framework16/home-manager/pkgs.nix
@@ -0,0 +1,81 @@
+{ pkgs, unstable, ... }:
+let
+    dotnetSDK = pkgs.dotnet-sdk_8;
+in
+{
+          home.packages = (with pkgs; [
+            kdePackages.kate
+            python3
+            rustup
+            poetry
+            pdm
+            virt-manager
+            bitwarden
+            gomplate
+            marktext
+            kdePackages.krdc
+            ungoogled-chromium
+            talosctl
+            libreoffice-qt
+            ffmpeg
+            xdg-desktop-portal
+            kdePackages.xdg-desktop-portal-kde
+            kdePackages.kdeconnect-kde
+            nodejs_20
+            kdePackages.plasma-workspace-wallpapers
+            kdePackages.plasma-browser-integration
+            nerdfonts
+            nextcloud-client
+            kdePackages.kolourpaint
+            vlc
+            kdePackages.filelight
+            kdePackages.skanpage
+            protonmail-bridge-gui
+            kdePackages.kweather
+            vimix-cursors
+            papirus-icon-theme
+            tela-icon-theme
+            fluent-icon-theme
+            dotnetSDK # Defined as let var
+            xournalpp
+            kdePackages.kcalc
+            protonmail-bridge
+            qownnotes
+            libsForQt5.kamoso
+            nil
+            dino
+            kdePackages.merkuro
+            kdePackages.akonadi
+            kdePackages.kdepim-runtime
+            kdePackages.k3b
+            kdePackages.ksshaskpass
+            kid3
+            go-task
+            gtkterm
+            drawio
+            android-tools
+            opentofu
+            kdePackages.kdenlive
+            p7zip # TODO: Remove once Ark works without it
+            go-task
+            podman-tui
+            dive
+            podman-compose
+            kdePackages.isoimagewriter
+            kubernetes-helm
+            helmfile
+            kubernetes-helmPlugins.helm-diff
+            kubernetes-helmPlugins.helm-git
+            kubectl
+            pyenv
+            webcord
+            netcoredbg
+            hunspell
+            hunspellDicts.en_US
+            jetbrains.rider
+            rclone
+        ])
+        ++ (with unstable; [
+            dbgate
+        ]);
+}
