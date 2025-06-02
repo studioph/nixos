90cc907e354f84508dc6aaa575bede2d56474a22
Paul Hutchings <dev@studiop.page>
2025-06-01 23:51:02 -0400

    Update system flake

--- framework16/system/flake.nix
+++ framework16/system/flake.nix
@@ -2,23 +2,16 @@
   description = "StudioP";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
     hardware.url = "github:nixos/nixos-hardware";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, hardware, ... }:
+  outputs = inputs@{ self, nixpkgs, hardware, ... }:
     let
       username = "paul";
     in
     {
-      defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
-
       nixosConfigurations.studiop = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs username; }; # allows access to flake inputs in nixos modules
         modules = [
