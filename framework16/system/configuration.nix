9b4f774f7b2c670eccee42b17b1485b29c0662ec
Paul Hutchings <dev@studiop.page>
2025-06-15 13:10:10 -0400

    Try enable systemd-resolved

--- framework16/system/configuration.nix
+++ framework16/system/configuration.nix
@@ -27,6 +27,7 @@
   # Enable networking
   networking.networkmanager.enable = true;
   networking.networkmanager.dns = "systemd-resolved";
+  services.resolved.enable = true;
 
   # Set your time zone.
   time.timeZone = "America/New_York";
